545751b481040187e22c61fe3f986e881bf99988
yoonisso <yoonissoppke@gmail.com>
2024-08-01 11:32:04 +0200

    INIT

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Erinnerungen-App',
+  webDir: 'dist'
+};
+
+export default config;
